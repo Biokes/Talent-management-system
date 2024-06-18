8c6da49350c6c0c5b1f44b53823d48bd166fc387
Victor-Maverick <victormsonter@gmail.com>
2024-06-18 01:16:29 +0100

    commit

--- talent_management_system/models.py
+++ talent_management_system/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    position = models.CharField(max_length=50)
+
+
+class Manager(Staff):
+    department_managed = models.CharField(max_length=255)
+
+
+class Employee(Staff):
+    manager_id = models.ForeignKey(Manager, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Skill(models.Model):
+    id = models.AutoField(primary_key=True)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    PROFICIENCY_LEVEL = [('B', 'BEGINNER'), ('I', 'INTERMEDIATE'), ('A', 'ADVANCED')]
+    proficiency = models.CharField(max_length=15, choices=PROFICIENCY_LEVEL, default='B')
+
+
